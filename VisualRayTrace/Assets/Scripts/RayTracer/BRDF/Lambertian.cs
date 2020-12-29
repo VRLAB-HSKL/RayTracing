a2670707a647262f078e7643705509d9e3731cc9
BenSaero <benedict.saerota@gmail.com>
2020-12-29 23:18:26 +0100

    Added BRDF class structure

---
+++ VisualRayTrace/Assets/Scripts/RayTracer/BRDF/Lambertian.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lambertian : AbstractBRDF
+{
+    private float _kd;
+    private Color _cd;    
+
+    public Lambertian() : base()
+    {
+        _kd = 0f;
+        _cd = new Color(0f, 0f, 0f);
+    }
+
+    public override Color F(RaycastHit hit, Vector3 wo, Vector3 wi)
+    {
+        return _kd * _cd * INV_PI;
+    }    
+
+    public override Color SampleF(RaycastHit hit, Vector3 wo, Vector3 wi)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override Color SampleF(RaycastHit hit, Vector3 wo, Vector3 wi, out float pdf)
+    {
+        Vector3 w = hit.normal;
+        Vector3 v = new Vector3(0.0034f, 1f, 0.0071f);
+        v.x = Mathf.Pow(v.x, w.x);
+        v.y = Mathf.Pow(v.y, w.y);
+        v.z = Mathf.Pow(v.z, w.z);
+        v = v.normalized;
+
+        Vector3 u = new Vector3(Mathf.Pow(v.x, w.x), Mathf.Pow(v.y, w.y), Mathf.Pow(v.z, w.z));
+
+        Vector3 sp = _sampler.SampleHemisphere();
+
+        wi = sp.x * u + sp.y * v + sp.z * w;
+        wi = wi.normalized;
+
+        // ToDo: Make sure this dot product is correct
+        pdf = Vector3.Dot(hit.normal, wi) * INV_PI;
+
+        return (_kd * _cd * INV_PI);
+    }
+
+    public override Color Rho(RaycastHit hit, Vector3 wo)
+    {
+        return _kd * _cd;
+    }
+}
---
+++ VisualRayTrace/Assets/Scripts/RayTracer/BRDF/PerfectSpecular.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerfectSpecular : AbstractBRDF
+{
+    private float _kr; // reflection coefficient
+    private Color _cr; // the reflection color
+    
+    public PerfectSpecular() : base()
+    {
+        _kr = 0f;
+        _cr = new Color(1f, 1f, 1f);
+    }
+
+    public override Color F(RaycastHit hit, Vector3 wi, Vector3 wo)
+    {
+        return Color.black;
+    }
+
+    public override Color Rho(RaycastHit hit, Vector3 wo)
+    {
+        return Color.black;
+    }
+
+    public override Color SampleF(RaycastHit hit, Vector3 wo, Vector3 wi)
+    {
+        // ToDo: Make sure this dot product is correct
+        float ndotwo = Vector3.Dot(hit.normal, wo);
+        wi = -wo + 2f * hit.normal * ndotwo;
+        // ToDo: Make sure this dot product is correct
+        return (_kr * _cr / Mathf.Abs(Vector3.Dot(hit.normal,wi)));
+        // why is this fabs? 
+        // kr would be a Fresnel term in a Fresnel reflector
+        // for transparency when ray hits inside surface?, if so it should go in Chapter 24
+    }
+
+    public override Color SampleF(RaycastHit hit, Vector3 wo, Vector3 wi, out float pdf)
+    {
+        // ToDo: Make sure this dot product is correct
+        float ndotwo = Vector3.Dot(hit.normal, wo);
+        wi = -wo + 2f * hit.normal * ndotwo;
+        // ToDo: Make sure this dot product is correct
+        pdf = Mathf.Abs(Vector3.Dot(hit.normal, wi));
+        return (_kr * _cr);
+    }
+}
