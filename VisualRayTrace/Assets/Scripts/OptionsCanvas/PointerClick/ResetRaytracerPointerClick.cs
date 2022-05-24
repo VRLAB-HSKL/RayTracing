87620d831ba4b950440f6931172416d1136f88c7
BenSaero <benedict.saerota@gmail.com>
2022-05-24 13:50:45 +0200

    Fixed main menu wall buttons

---
+++ VisualRayTrace/Assets/Scripts/OptionsCanvas/PointerClick/ResetRaytracerPointerClick.cs
@@ -0,0 +1,23 @@
+﻿using HTC.UnityPlugin.Vive;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace OptionsCanvas
+{
+    public class ResetRaytracerPointerClick : MonoBehaviour, IPointerClickHandler
+    {
+        /// <summary>
+        /// Custom vive activation button
+        /// </summary>
+        public ControllerButton ActivationButton;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.IsViveButton(ActivationButton))
+            {
+                var rt = FindObjectOfType<RayTracerUnity>();
+                rt.ResetRaytracer();
+            }
+        }
+    }
+}
---
+++ VisualRayTrace/Assets/Scripts/OptionsCanvas/PointerClick/SaveImagePointerClick.cs
@@ -0,0 +1,22 @@
+﻿using HTC.UnityPlugin.Vive;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace OptionsCanvas
+{
+    public class SaveImagePointerClick : MonoBehaviour, IPointerClickHandler
+    {
+        /// <summary>
+        /// Custom vive activation button
+        /// </summary>
+        public ControllerButton ActivationButton;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.IsViveButton(ActivationButton))
+            {
+                
+            }
+        }
+    }
+}
